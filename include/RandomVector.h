cef63f16eabb4637d4159e11d911651c742977c7
knippsch <B.Knippschild@gmx.de>
2015-07-15 12:22:50 +0200

    random vectors included

---
+++ include/RandomVector.h
@@ -0,0 +1,80 @@
+#ifndef RANDOMVECTOR_H_
+#define RANDOMVECTOR_H_
+
+#include <complex>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "ranlxs.h"
+
+namespace LapH {
+
+typedef std::complex<double> cmplx;
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+class RandomVector {
+
+private:
+  // the random vector
+  std::vector<cmplx> vec;
+  size_t nb_entities;
+  size_t length;
+
+public:
+  // standard ctor and dtor are enough - all else is handled by std::vector
+  RandomVector() : vec(0, cmplx(0.0,0.0)) { };
+  // NOTE: It is assumed that all random vector have the same length!
+  RandomVector(const size_t nb_entities, const size_t length) : 
+                vec(nb_entities*length, cmplx(0.0,0.0)), 
+                nb_entities(nb_entities),
+                length(length) {
+    std::cout << "\tRandom vectors initialised" << std::endl;
+  };
+  ~RandomVector() {};
+
+  // () operator to directly access the elements of vec
+  inline cmplx operator()(const size_t entity, const size_t entry) const {
+    return vec[entity*length+entry];
+  }
+
+  // computes the random vectors for the sources
+  // input: entity -> the random vector you want to set
+  //        seed   -> seed for the random vector
+  //        length -> length of the random vector
+  void set(const size_t entity, const int seed);
+  // computes the random vectors for the sources and stores them
+  // input: entity   -> the random vector you want to set
+  //        seed     -> seed for the random vector
+  //        length   -> length of the random vector
+  //        filename -> the random vector is direlty stored in this file
+  void set(const size_t entity, const int seed, const std::string& filename);
+  // writing all random vector to some file
+  // input: filename -> the filename
+  void write_random_vector(const std::string& filename) const;
+  // writing one random vector (entity) to some file
+  // input: entity   -> the random vector which will be stored on disk
+  //        filename -> the filename
+  void write_random_vector(const size_t entity, 
+                           const std::string& filename) const;
+  // reading all random vectors from some file
+  // input: filename -> the filename
+  void read_random_vector(const std::string& filename);
+  // reading one random vector from some file
+  // input: entity   -> the place where this random vector will be stored
+  //        filename -> the filename
+  void read_random_vector(const size_t entity, const std::string& filename);
+  // rading random vectors where each vector is stored in a different file
+  // input: filename_list -> vector which contains all file names
+  void read_random_vectors_from_separate_files(
+                                 const std::vector<std::string>& filename_list);
+
+};
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+} // end of namespace
+
+#endif // RANDOMVECTOR_H_ 
---
+++ modules/RandomVector.cpp
@@ -0,0 +1,122 @@
+#include "RandomVector.h"
+
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+void LapH::RandomVector::set(const size_t entity, const int seed) {
+
+  // initialisation of the rando vector to create Z2 random vector
+  rlxs_init(0, seed);
+  std::vector<float> rnd(2*length);
+  ranlxs(&(rnd[0]), 2*length);
+
+  // generating a Z_2 source
+  for(size_t i = 0; i < length; ++i){
+    const double sqrt2 = 0.5*sqrt(2.0);
+    double re, im;
+    if (rnd[2*i] < 0.5)
+      re = -sqrt2;
+    else
+      re = sqrt2;
+    if (rnd[2*i + 1] < 0.5)
+      im = -sqrt2;
+    else
+      im = sqrt2;
+    vec[entity*length + i] = cmplx(re, im);
+  }
+
+}
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+void LapH::RandomVector::set(const size_t entity, const int seed, 
+                             const std::string& filename) {
+  set(entity, seed);
+  write_random_vector(filename);
+
+}
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+void LapH::RandomVector::write_random_vector(
+                                       const std::string& filename) const {
+  // writing random vector to file
+  FILE* fp = NULL;
+  if((fp = fopen(filename.c_str(), "wb")) == NULL){
+    std::cout << "failed to open file to write random vector: " 
+              << filename << "\n" << std::endl;
+    exit(0);
+  }   
+  int check_read_in = fwrite(&(vec[0]), sizeof(cmplx), vec.size(), fp);
+  if(check_read_in !=  (int) vec.size())
+    std::cout << "It seems that not all data were written to: "
+              << filename.c_str() << "\n" << std::endl;
+  fclose(fp);
+
+} 
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+void LapH::RandomVector::write_random_vector(const size_t entity,
+                                       const std::string& filename) const {
+  // writing random vector to file
+  FILE* fp = NULL;
+  if((fp = fopen(filename.c_str(), "wb")) == NULL){
+    std::cout << "failed to open file to write random vector: " 
+              << filename << "\n" << std::endl;
+    exit(0);
+  }   
+  int check_read_in = fwrite(&(vec[entity*length]), sizeof(cmplx), length, fp);
+  if(check_read_in != length)
+    std::cout << "It seems that not all data were written to: "
+              << filename.c_str() << "\n" << std::endl;
+  fclose(fp);
+
+} 
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+void LapH::RandomVector::read_random_vector(const std::string& filename) {
+
+  // open file for reading
+  FILE *fp = NULL;
+  if((fp = fopen(filename.c_str(), "rb")) == NULL){
+    std::cout << "failed to open file to read random vector: " 
+              << filename << "\n" << std::endl;
+    exit(0);
+  }   
+  // reading data
+  int check_read_in = fread(&(vec[0]), sizeof(cmplx), vec.size(), fp);
+  if(check_read_in !=  (int) vec.size())
+    std::cout << "It seems that not all data are written to: "
+              << filename.c_str() << "\n" << std::endl;
+
+}
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+void LapH::RandomVector::read_random_vector(const size_t entity, 
+                                            const std::string& filename) {
+  // open file for reading
+  FILE *fp = NULL;
+  if((fp = fopen(filename.c_str(), "rb")) == NULL){
+    std::cout << "failed to open file to read random vector: " 
+              << filename << "\n" << std::endl;
+    exit(0);
+  }   
+  // reading data
+  int check_read_in = fread(&(vec[entity*length]), sizeof(cmplx), length, fp);
+  if(check_read_in !=  length)
+    std::cout << "It seems that not all data are written to: "
+              << filename.c_str() << "\n" << std::endl;
+
+}
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+void LapH::RandomVector::read_random_vectors_from_separate_files(
+                                const std::vector<std::string>& filename_list) {
+
+  if(filename_list.size() != nb_entities)
+    std::cout << "Problem when reading random vectors: The number of read "
+              << "random vectors is not the same as the expected one!" 
+              << std::endl;
+  for(size_t i = 0; i < filename_list.size(); i++)
+    read_random_vector(i, filename_list[i]);
+}
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+ 
