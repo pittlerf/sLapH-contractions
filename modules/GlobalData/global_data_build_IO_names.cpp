671373d1e95a38286ae1437f36b525dd26fd9ce6
Christopher Helmes <helmes@hiskp.uni-bonn.de>
2015-10-28 09:53:52 +0100

    adapted Makefile to qbig and global data for right quark types

--- modules/GlobalData/global_data_build_IO_names.cpp
+++ modules/GlobalData/global_data_build_IO_names.cpp
@@ -16,8 +16,9 @@ static std::vector<std::string> create_rnd_vector_file_names (
     for(int rnd_vec_i = 0; rnd_vec_i < q.number_of_rnd_vec; ++rnd_vec_i){
       // building paths and filenames for rnd vecs
       sprintf(temp1, "cnfg%d/rnd_vec_%01d/", config, rnd_vec_i);
-      sprintf(temp2, "randomvector.rndvecnb%02d.u.nbev%04d.%04d", rnd_vec_i, 
-                                                     nb_of_eigen_vec, config);
+      sprintf(temp2, "randomvector.rndvecnb%02d.%s.nbev%04d.%04d", rnd_vec_i,
+                                                     q.type.c_str(), nb_of_eigen_vec,
+                                                     config);
       filename_list.push_back(q.path + "/" + temp1 + temp2);
     }
   }
@@ -41,9 +42,9 @@ static std::vector<std::string> create_perambulator_file_names (
       sprintf(temp1, "cnfg%d/rnd_vec_%01d/", config, rnd_vec_i);
 
       sprintf(temp2,
-          "perambulator.rndvecnb%02d.u.TsoB%04d.VsoI%04d.DsoF%1d.TsiF%04d."
+          "perambulator.rndvecnb%02d.%s.TsoB%04d.VsoI%04d.DsoF%1d.TsiF%04d."
           "SsiF%d.DsiF4.CsiF3.smeared1.%05d", 
-          rnd_vec_i, Lt / q.number_of_dilution_T, q.number_of_dilution_E,
+          rnd_vec_i, q.type.c_str(), Lt / q.number_of_dilution_T, q.number_of_dilution_E,
           q.number_of_dilution_D, Lt, 13824, config);
       filename_list.push_back(q.path + "/" + temp1 + temp2);
     }
