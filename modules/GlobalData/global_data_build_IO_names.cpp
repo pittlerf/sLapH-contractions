9e19301377e614709db5e8e93916b99aece38663
maowerner <s6mawern@uni-bonn.de>
2017-03-25 05:49:39 +0100

    compiles on Qbig for 24^3

--- modules/GlobalData/global_data_build_IO_names.cpp
+++ modules/GlobalData/global_data_build_IO_names.cpp
@@ -15,7 +15,7 @@ static std::vector<std::string> create_rnd_vector_file_names (
 
     for(int rnd_vec_i = 0; rnd_vec_i < q.number_of_rnd_vec; ++rnd_vec_i){
       // building paths and filenames for rnd vecs
-      sprintf(temp1, "cnfg%d/rnd_vec_%01d/", config, rnd_vec_i);
+      sprintf(temp1, "cnfg%04d/rnd_vec_%02d/", config, rnd_vec_i);
 //      sprintf(temp1, "");
       sprintf(temp2, "randomvector.rndvecnb%02d.u.nbev%04d.%04d", rnd_vec_i, 
                                                      nb_of_eigen_vec, config);
@@ -40,12 +40,12 @@ static std::vector<std::string> create_perambulator_file_names (
 
     for(int rnd_vec_i = 0; rnd_vec_i < q.number_of_rnd_vec; ++rnd_vec_i){
       // data path for qbig contractions
-      sprintf(temp1, "cnfg%d/rnd_vec_%01d/", config, rnd_vec_i);
+      sprintf(temp1, "cnfg%04d/rnd_vec_%02d/", config, rnd_vec_i);
       //sprintf(temp1, "");
 
       sprintf(temp2,
           "perambulator.rndvecnb%02d.u.TsoB%04d.VsoI%04d.DsoF%1d.TsiF%04d."
-          "SsiF%d.DsiF4.CsiF3.smeared1.%05d", 
+          "SsiF%d.DsiF4.CsiF3.smeared0.%05d", 
           rnd_vec_i, Lt / q.number_of_dilution_T, q.number_of_dilution_E,
           q.number_of_dilution_D, Lt, 13824, config);
       filename_list.push_back(q.path + "/" + temp1 + temp2);
