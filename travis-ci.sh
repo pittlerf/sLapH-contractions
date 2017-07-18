98bd37778cddba1b40e22bdd67f56756fb738ce9
Martin Ueding <dev@martin-ueding.de>
2017-07-18 15:49:29 +0200

    Add Travis CI files

---
+++ travis-ci.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Copyright © 2017 Martin Ueding <dev@martin-ueding.de>
+
+set -e
+set -u
+set -x
+
+ubuntu_packages=(
+    gcc-6 g++-6
+    ccache
+    cmake
+    libhdf5-dev 
+    libeigen3-dev
+)
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt-get update
+sudo apt-get install -y "${ubuntu_packages[@]}"
+
+sourcedir="$(pwd)"
+
+cd ..
+mkdir build
+cd build
+
+cmake "$sourcedir"
