cb44e4c60b6014a435c234c90515badb563dd1b6
Martin Ueding <dev@martin-ueding.de>
2017-12-19 16:54:10 +0100

    Throw out some sprintf calls, 2

--- modules/GlobalData/global_data_build_IO_names.cpp
+++ modules/GlobalData/global_data_build_IO_names.cpp
@@ -49,14 +49,15 @@ static std::vector<std::string> create_perambulator_file_names (
 
     for(int rnd_vec_i = 0; rnd_vec_i < q.number_of_rnd_vec; ++rnd_vec_i){
       // data path for qbig contractions
-      sprintf(temp1, "cnfg%04d/rnd_vec_%02d/", config, rnd_vec_i);
-      sprintf(temp2,
-          "perambulator.rndvecnb%02d.%s.Tso%c%04d.Vso%c%04d.Dso%c%1d.TsiF%04d."
-          "SsiF%04d.DsiF4.CsiF3.smeared0.%05d", 
-          rnd_vec_i, q.type.c_str(), dil_scheme_T, Lt / q.number_of_dilution_T, 
-          dil_scheme_E, q.number_of_dilution_E, dil_scheme_D, q.number_of_dilution_D, 
-          Lt, Lx*Ly*Lz, config);
-      filename_list.push_back(q.path + "/" + temp1 + temp2);
+      auto const path =
+          (boost::format("%s/cnfg%04d/rnd_vec_%02d/"
+                         "perambulator.rndvecnb%02d.%s.Tso%c%04d.Vso%c%04d.Dso%c%1d.TsiF%"
+                         "04d.SsiF%04d.DsiF4.CsiF3.smeared0.%05d") %
+           q.path % config % rnd_vec_i % rnd_vec_i % q.type.c_str() % dil_scheme_T %
+           (Lt / q.number_of_dilution_T) % dil_scheme_E % q.number_of_dilution_E %
+           dil_scheme_D % q.number_of_dilution_D % Lt % (Lx * Ly * Lz) % config)
+              .str();
+      filename_list.push_back(path);
     }
   }
   return filename_list;
