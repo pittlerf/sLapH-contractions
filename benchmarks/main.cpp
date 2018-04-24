44dc623d175947283281b76914a8322e3b501ec7
Martin Ueding <dev@martin-ueding.de>
2018-04-24 15:30:49 +0200

    Prepare a benchmark

--- benchmarks/main.cpp
+++ benchmarks/main.cpp
@@ -1,3 +1,5 @@
+//#include "OperatorsForMesons.h"
+
 #include <benchmark/benchmark.h>
 
 #include <cmath>
@@ -9,5 +11,15 @@ static void BM_exp(benchmark::State &state) {
 }
 
 BENCHMARK(BM_exp);
+
+#if 0
+static void BM_create_momenta_old(benchmark::State &state) {
+  std::vector<VdaggerVQuantumNumbers> vdaggerv_lookup{
+    {0, {1, 2, 3}, {std::make_pair('>', 'x')}}
+  };
+}
+
+BENCHMARK(BM_create_momenta_old);
+#endif
 
 BENCHMARK_MAIN();
