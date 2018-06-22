da5b5adcc501a78ca9ed6aa34b976739fa0257bc
Markus Werner <s6mawern@uni-bonn.de>
2018-06-22 12:25:48 +0200

    Some comments

--- modules/DilutedTraceFactory.cpp
+++ modules/DilutedTraceFactory.cpp
@@ -13,20 +13,20 @@ void DilutedTraceCollection<DilutedFactorType::Q0, DilutedFactorType::Q2, 2>::bu
       q0[{t2}].at({c_look.lookup[1]}), q2v[{b2, t1, b2}].at({c_look.lookup[0]}));
 }
 
+// dummy must be passed to keep the function call consistent, but it is a reference to 
+// the same object.
 template <>
 void DilutedTraceCollection<DilutedFactorType::Q1, DilutedFactorType::Q1, 2>::build(
     DilutedFactorFactory<DilutedFactorType::Q1> &q1,
-    DilutedFactorFactory<DilutedFactorType::Q1> &q1_dummy,
+    DilutedFactorFactory<DilutedFactorType::Q1> &dummy,
                  DiagramIndex const &c_look,
                  int const t1,
                  int const t2,
                  int const b1,
                  int const b2) {
 
-  auto tmp1 = q1[{t1, b2}].at({c_look.lookup[0]});
-  auto tmp2 = q1[{t2, b1}].at({c_look.lookup[1]});
-
-  tr[{t1,t2}][c_look.id] = factor_to_trace(tmp1, tmp2);
+  tr[{t1,t2}][c_look.id] = factor_to_trace(q1[{t1, b2}].at({c_look.lookup[0]}), 
+      q1[{t2, b1}].at({c_look.lookup[1]}));
 }
 
 template class DilutedTraceCollection<DilutedFactorType::Q0, DilutedFactorType::Q2, 2>;
