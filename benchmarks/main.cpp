049b05d95f8f6230bffd7a7119d0471d25d24e20
Martin Ueding <dev@martin-ueding.de>
2018-11-07 11:46:45 +0100

    Remove unused variable

--- benchmarks/main.cpp
+++ benchmarks/main.cpp
@@ -3,7 +3,7 @@
 #include <benchmark/benchmark.h>
 
 static void BM_create_momenta_old(benchmark::State &state) {
-  ssize_t const Lx = 32, Ly = 32, Lz = 32, Lt = 96;
+  ssize_t const Lx = 32, Ly = 32, Lz = 32;
   std::vector<VdaggerVQuantumNumbers> vdaggerv_lookup{{0, {1, 2, 3}, {}}};
   array_cd_d2 momentum(boost::extents[1][Lx*Ly*Lz]);
 
@@ -15,7 +15,7 @@ static void BM_create_momenta_old(benchmark::State &state) {
 BENCHMARK(BM_create_momenta_old);
 
 static void BM_create_momenta_new(benchmark::State &state) {
-  ssize_t const Lx = 32, Ly = 32, Lz = 32, Lt = 96;
+  ssize_t const Lx = 32, Ly = 32, Lz = 32;
   std::vector<VdaggerVQuantumNumbers> vdaggerv_lookup{{0, {1, 2, 3}, {}}};
   array_cd_d2 momentum(boost::extents[1][Lx*Ly*Lz]);
 
