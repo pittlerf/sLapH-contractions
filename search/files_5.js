c7b7581895968da105051793722ec1ec67376af1
Martin Ueding <dev@martin-ueding.de>
2018-11-12 00:01:10 +0100

    New documentation

--- search/files_5.js
+++ search/files_5.js
@@ -7,7 +7,9 @@ var searchData=
   ['global_5fdata_2ecpp',['global_data.cpp',['../global__data_8cpp.html',1,'']]],
   ['global_5fdata_2ehpp',['global_data.hpp',['../global__data_8hpp.html',1,'']]],
   ['global_5fdata_5fbuild_5fio_5fnames_2ecpp',['global_data_build_IO_names.cpp',['../global__data__build___i_o__names_8cpp.html',1,'']]],
+  ['global_5fdata_5fbuild_5fio_5fnames_2ehpp',['global_data_build_IO_names.hpp',['../global__data__build___i_o__names_8hpp.html',1,'']]],
   ['global_5fdata_5finput_5fhandling_2ecpp',['global_data_input_handling.cpp',['../global__data__input__handling_8cpp.html',1,'']]],
+  ['global_5fdata_5finput_5fhandling_2ehpp',['global_data_input_handling.hpp',['../global__data__input__handling_8hpp.html',1,'']]],
   ['global_5fdata_5finput_5fhandling_5futils_2ecpp',['global_data_input_handling_utils.cpp',['../global__data__input__handling__utils_8cpp.html',1,'']]],
   ['global_5fdata_5ftypedefs_2ehpp',['global_data_typedefs.hpp',['../global__data__typedefs_8hpp.html',1,'']]],
   ['global_5fdata_5futils_2ehpp',['global_data_utils.hpp',['../global__data__utils_8hpp.html',1,'']]]
