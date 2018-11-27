f15c17d86c56c02d8516b0034156d5fd0a90d202
Martin Ueding <dev@martin-ueding.de>
2018-11-27 19:32:59 +0100

    Add a DilutedTrace4Factory

--- include/DilutedTraceFactory.hpp
+++ include/DilutedTraceFactory.hpp
@@ -120,3 +120,56 @@ struct DilutedTrace3Factory {
   DilutionScheme const &dilution_scheme;
   std::map<Key, Value> Tr;
 };
+
+template <DilutedFactorType qlt1,
+          DilutedFactorType qlt2,
+          DilutedFactorType qlt3,
+          DilutedFactorType qlt4,
+          size_t rvecs>
+struct DilutedTrace4Factory {
+ public:
+  /** num_times is the sum of times of contained factors -1 for each continuity
+   *  condition of the quarkline diagram
+   */
+  static constexpr int num_times = DilutedFactorTypeTraits<qlt1>::num_times +
+                                   DilutedFactorTypeTraits<qlt2>::num_times +
+                                   DilutedFactorTypeTraits<qlt3>::num_times +
+                                   DilutedFactorTypeTraits<qlt4>::num_times - 2;
+
+  using Key = std::array<int, num_times>;
+  using Value = DilutedTraces<rvecs>;
+
+  DilutedTrace4Factory(DilutedFactorFactory<qlt1> &_df1,
+                       DilutedFactorFactory<qlt2> &_df2,
+                       DilutedFactorFactory<qlt3> &_df3,
+                       DilutedFactorFactory<qlt4> &_df4,
+                       std::vector<Indices> const &_dic,
+                       DilutionScheme const &_ds)
+      : df1(_df1),
+        df2(_df2),
+        df3(_df3),
+        df4(_df4),
+        diagram_index_collection(_dic),
+        dilution_scheme(_ds) {}
+
+  Value const &operator[](Key const &key) {
+    if (Tr.count(key) == 0) {
+      build(key);
+    }
+
+    return Tr.at(key);
+  }
+
+  void build(Key const &time_key);
+
+  void clear() { Tr.clear(); }
+
+ private:
+  DilutedFactorFactory<qlt1> &df1;
+  DilutedFactorFactory<qlt2> &df2;
+  DilutedFactorFactory<qlt3> &df3;
+  DilutedFactorFactory<qlt4> &df4;
+  std::vector<Indices> const &diagram_index_collection;
+  DilutionScheme const &dilution_scheme;
+  std::map<Key, Value> Tr;
+};
--- src/DilutedTraceFactory.cpp
+++ src/DilutedTraceFactory.cpp
@@ -1,6 +1,46 @@
 #include "DilutedTraceFactory.hpp"
 
 template <>
+void DilutedTrace1Factory<DilutedFactorType::Q1, 1>::build(Key const &time_key) {
+  auto t = time_key[0];
+  auto b = dilution_scheme.time_to_block(t);
+
+  for (ssize_t i = 0; i != ssize(diagram_index_collection); ++i) {
+    const auto &c_look = diagram_index_collection[i];
+    Tr[{t}][i] = factor_to_trace(df[{t, b}].at({c_look[0]}));
+  }
+}
+
+template <>
+void DilutedTrace2Factory<DilutedFactorType::Q1, DilutedFactorType::Q1, 2>::build(
+    Key const &time_key) {
+  auto t1 = time_key[0];
+  auto t2 = time_key[1];
+  auto b1 = dilution_scheme.time_to_block(t1);
+  auto b2 = dilution_scheme.time_to_block(t2);
+
+  for (ssize_t i = 0; i != ssize(diagram_index_collection); ++i) {
+    const auto &c_look = diagram_index_collection[i];
+    Tr[{t1, t2}][i] = factor_to_trace(df1[{t1, b2}].at({c_look[0]}),
+                                      df2[{t2, b1}].at({c_look[1]}));
+  }
+}
+
+template <>
+void DilutedTrace2Factory<DilutedFactorType::Q0, DilutedFactorType::Q2, 2>::build(
+    Key const &time_key) {
+  auto t1 = time_key[0];
+  auto t2 = time_key[1];
+  auto b2 = dilution_scheme.time_to_block(t2);
+
+  for (ssize_t i = 0; i != ssize(diagram_index_collection); ++i) {
+    const auto &c_look = diagram_index_collection[i];
+    Tr[{t1, t2}][i] = factor_to_trace(df1[{t2}].at({c_look[1]}),
+                                      df2[{b2, t1, b2}].at({c_look[0]}));
+  }
+}
+
+template <>
 void DilutedTrace3Factory<DilutedFactorType::Q1,
                           DilutedFactorType::Q1,
                           DilutedFactorType::Q1,
@@ -24,42 +64,28 @@ void DilutedTrace3Factory<DilutedFactorType::Q1,
 }
 
 template <>
-void DilutedTrace2Factory<DilutedFactorType::Q0, DilutedFactorType::Q2, 2>::build(
-    Key const &time_key) {
-  auto t1 = time_key[0];
-  auto t2 = time_key[1];
-  auto b2 = dilution_scheme.time_to_block(t2);
+void DilutedTrace4Factory<DilutedFactorType::Q1,
+                          DilutedFactorType::Q1,
+                          DilutedFactorType::Q1,
+                          DilutedFactorType::Q1,
+                          4>::build(Key const &time_key) {
+  auto const t1 = time_key[0];
+  auto const t2 = time_key[1];
+  auto const b1 = dilution_scheme.time_to_block(t1);
+  auto const b2 = dilution_scheme.time_to_block(t2);
 
+  DilutedFactors<2, 1> L1;
+  DilutedFactors<2, 1> L2;
   for (ssize_t i = 0; i != ssize(diagram_index_collection); ++i) {
     const auto &c_look = diagram_index_collection[i];
-    Tr[{t1, t2}][i] = factor_to_trace(df1[{t2}].at({c_look[1]}),
-                                      df2[{b2, t1, b2}].at({c_look[0]}));
+    multiply<1, 1, 0, 0>(L1, {c_look[3], c_look[0]}, df4[{t1, b1}], df1[{t1, b2}]);
+    multiply<1, 1, 0, 0>(L2, {c_look[1], c_look[2]}, df2[{t2, b2}], df3[{t2, b1}]);
   }
-}
-
-template <>
-void DilutedTrace2Factory<DilutedFactorType::Q1, DilutedFactorType::Q1, 2>::build(
-    Key const &time_key) {
-  auto t1 = time_key[0];
-  auto t2 = time_key[1];
-  auto b1 = dilution_scheme.time_to_block(t1);
-  auto b2 = dilution_scheme.time_to_block(t2);
 
   for (ssize_t i = 0; i != ssize(diagram_index_collection); ++i) {
     const auto &c_look = diagram_index_collection[i];
-    Tr[{t1, t2}][i] = factor_to_trace(df1[{t1, b2}].at({c_look[0]}),
-                                      df2[{t2, b1}].at({c_look[1]}));
-  }
-}
-
-template <>
-void DilutedTrace1Factory<DilutedFactorType::Q1, 1>::build(Key const &time_key) {
-  auto t = time_key[0];
-  auto b = dilution_scheme.time_to_block(t);
-
-  for (ssize_t i = 0; i != ssize(diagram_index_collection); ++i) {
-    const auto &c_look = diagram_index_collection[i];
-    Tr[{t}][i] = factor_to_trace(df[{t, b}].at({c_look[0]}));
+    Tr[{t1, t2}][i] =
+        factor_to_trace(L1[{c_look[3], c_look[0]}], L1[{c_look[1], c_look[2]}]);
   }
 }
 
@@ -70,3 +96,8 @@ template class DilutedTrace3Factory<DilutedFactorType::Q1,
                                     DilutedFactorType::Q1,
                                     DilutedFactorType::Q1,
                                     3>;
+template class DilutedTrace4Factory<DilutedFactorType::Q1,
+                                    DilutedFactorType::Q1,
+                                    DilutedFactorType::Q1,
+                                    DilutedFactorType::Q1,
+                                    4>;
