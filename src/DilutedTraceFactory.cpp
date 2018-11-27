246ecba0203f2e7012c2ea1fee56dcdfe47f7d06
Martin Ueding <dev@martin-ueding.de>
2018-11-27 18:51:57 +0100

    Introduce a DilutedTrace3Factory for C30

--- src/DilutedTraceFactory.cpp
+++ src/DilutedTraceFactory.cpp
@@ -1,6 +1,29 @@
 #include "DilutedTraceFactory.hpp"
 
 template <>
+void DilutedTrace3Factory<DilutedFactorType::Q1,
+                          DilutedFactorType::Q1,
+                          DilutedFactorType::Q1,
+                          3>::build(Key const &time_key) {
+  auto const t1 = time_key[0];
+  auto const t2 = time_key[1];
+  auto const b1 = dilution_scheme.time_to_block(t1);
+  auto const b2 = dilution_scheme.time_to_block(t2);
+
+  DilutedFactors<2, 1> L1;
+  for (ssize_t i = 0; i != ssize(diagram_index_collection); ++i) {
+    const auto &c_look = diagram_index_collection[i];
+    multiply<1, 1, 0, 0>(L1, {c_look[2], c_look[0]}, df1[{t1, b1}], df2[{t1, b2}]);
+  }
+
+  for (ssize_t i = 0; i != ssize(diagram_index_collection); ++i) {
+    const auto &c_look = diagram_index_collection[i];
+    Tr[{t1, t2}][i] =
+        factor_to_trace(L1[{c_look[2], c_look[0]}], df3[{t2, b1}].at({c_look[1]}));
+  }
+}
+
+template <>
 void DilutedTrace2Factory<DilutedFactorType::Q0, DilutedFactorType::Q2, 2>::build(
     Key const &time_key) {
   auto t1 = time_key[0];
@@ -40,6 +63,10 @@ void DilutedTrace1Factory<DilutedFactorType::Q1, 1>::build(Key const &time_key) 
   }
 }
 
+template class DilutedTrace1Factory<DilutedFactorType::Q1, 1>;
 template class DilutedTrace2Factory<DilutedFactorType::Q0, DilutedFactorType::Q2, 2>;
 template class DilutedTrace2Factory<DilutedFactorType::Q1, DilutedFactorType::Q1, 2>;
-template class DilutedTrace1Factory<DilutedFactorType::Q1, 1>;
+template class DilutedTrace3Factory<DilutedFactorType::Q1,
+                                    DilutedFactorType::Q1,
+                                    DilutedFactorType::Q1,
+                                    3>;
