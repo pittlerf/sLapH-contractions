61fbb228733c44c70d932cb77fe3444cd7980c1b
Martin Ueding <dev@martin-ueding.de>
2018-12-04 12:19:41 +0100

    Add missing file

---
+++ src/DilutedFactor.cpp
@@ -0,0 +1,276 @@
+#include "DilutedFactor.hpp"
+
+bool has_intersection(SmallVectorRndId const &left, SmallVectorRndId const &right) {
+  SmallVectorRndId intersection;
+
+  std::set_intersection(std::begin(left),
+                        std::end(left),
+                        std::begin(right),
+                        std::end(right),
+                        std::back_inserter(intersection));
+  return intersection.size() > 0;
+}
+
+void merge_append(SmallVectorRndId &data, SmallVectorRndId const &addition) {
+  auto const old_end = std::end(data);
+  std::copy(std::begin(addition), std::end(addition), std::back_inserter(data));
+  std::inplace_merge(std::begin(data), old_end, std::end(data));
+}
+
+void merge_push_back(SmallVectorRndId &data, RndId const &addition) {
+  auto const old_end = std::end(data);
+  data.push_back(addition);
+  std::inplace_merge(std::begin(data), old_end, std::end(data));
+}
+
+std::vector<DilutedFactor> operator*(std::vector<DilutedFactor> const &left_vec,
+                                     std::vector<DilutedFactor> const &right_vec) {
+  assert(left_vec.size() > 0);
+  assert(right_vec.size() > 0);
+
+  std::vector<DilutedFactor> result_vec;
+
+  for (auto const &left : left_vec) {
+    auto const inner_rnd_id = left.ric.second;
+
+    for (auto const &right : right_vec) {
+      // We want to make the inner and outer indices differ. The inner indices need to
+      // match because the product would not make sense otherwise.
+      bool const is_allowed =
+          inner_rnd_id == right.ric.first && left.ric.first != right.ric.second;
+      if (!is_allowed) {
+        continue;
+      }
+
+      // We also need to be careful to not combine factors which have common used random
+      // vector indices.
+      if (has_intersection(left.used_rnd_ids, right.used_rnd_ids)) {
+        continue;
+      }
+
+      // We want to keep track of the indices that have been contracted away. These are
+      // all the ones from the left factor, all the ones from the right factor and the one
+      // that we are contracting over right now.
+      SmallVectorRndId used;
+      used.push_back(inner_rnd_id);
+      merge_append(used, left.used_rnd_ids);
+      merge_append(used, right.used_rnd_ids);
+
+      result_vec.push_back({left.data * right.data,
+                            std::make_pair(left.ric.first, right.ric.second),
+                            used});
+    }
+  }
+
+  if (result_vec.size() == 0) {
+    throw std::runtime_error(
+        "vector<DilutedFactor> operator*(vector<DilutedFactor>, vector<DilutedFactor>) "
+        "has an empty result.");
+  }
+
+  return result_vec;
+}
+
+Complex trace(std::vector<DilutedFactor> const &left_vec,
+              std::vector<DilutedFactor> const &right_vec) {
+  assert(left_vec.size() > 0);
+  assert(right_vec.size() > 0);
+
+  Complex result(0.0, 0.0);
+
+  int num_summands = 0;
+
+  LT_ULTRA_FINE_DECLARE;
+
+  for (auto const &left : left_vec) {
+    auto const outer_rnd_id = left.ric.first;
+    auto const inner_rnd_id = left.ric.second;
+
+    LT_ULTRA_FINE_START;
+    Eigen::MatrixXcd right_sum(
+        Eigen::MatrixXcd::Zero(left.data.rows(), left.data.cols()));
+
+    for (auto const &right : right_vec) {
+      // We want to make the inner and outer indices match. The inner indices need to
+      // match because the product would not make sense otherwise. The outer indices must
+      // match since we want to be able to take the trace over the result. The second
+      // condition is where this differs from the other multiplication operator.
+      bool const is_allowed =
+          inner_rnd_id == right.ric.first && outer_rnd_id == right.ric.second;
+      if (!is_allowed) {
+        continue;
+      }
+
+      // We also need to be careful to not combine factors which have common used random
+      // vector indices.
+      if (has_intersection(left.used_rnd_ids, right.used_rnd_ids)) {
+        continue;
+      }
+
+      // The right sides that we encounter at this point have the same left and right
+      // random vector indices. They may differ in the set of used random vector indices.
+      // But since we do not plan to contract the result with more DilutedFactor
+      // instances, we do not care to preserve the information about the used random
+      // vector indices. Therefore we can sum all these elements up to have less
+      // multiplications to do.
+      right_sum += right.data;
+      ++num_summands;
+    }
+    LT_ULTRA_FINE_STOP;
+    LT_ULTRA_FINE_PRINT("[DilutedFactor::trace] right_sum");
+
+    LT_ULTRA_FINE_START;
+    auto const &product = left.data * right_sum;
+    result += product.trace();
+    LT_ULTRA_FINE_STOP;
+    LT_ULTRA_FINE_PRINT("[DilutedFactor::trace] product_trace");
+
+  }  // for(left_vec)
+
+  if (num_summands == 0) {
+    throw std::runtime_error(
+        "cmplx trace(vector<DilutedFactor>, vector<DilutedFactor>) has used zero "
+        "summands.");
+  }
+
+  return result / static_cast<double>(num_summands);
+}
+
+std::vector<DilutedTrace> factor_to_trace(std::vector<DilutedFactor> const &left_vec,
+                                          std::vector<DilutedFactor> const &right_vec) {
+  assert(left_vec.size() > 0);
+  assert(right_vec.size() > 0);
+
+  std::vector<DilutedTrace> result_vec;
+
+  LT_ULTRA_FINE_DECLARE;
+
+  for (auto const &left : left_vec) {
+    auto const outer_rnd_id = left.ric.first;
+    auto const inner_rnd_id = left.ric.second;
+
+    LT_ULTRA_FINE_START;
+    for (auto const &right : right_vec) {
+      // We want to make the inner and outer indices match. The inner indices need to
+      // match because the product would not make sense otherwise. The outer indices must
+      // match since we want to be able to take the trace over the result. The second
+      // condition is where this differs from the other multiplication operator.
+      bool const is_allowed =
+          inner_rnd_id == right.ric.first && outer_rnd_id == right.ric.second;
+      if (!is_allowed) {
+        continue;
+      }
+
+      // We also need to be careful to not combine factors which have common used random
+      // vector indices.
+      if (has_intersection(left.used_rnd_ids, right.used_rnd_ids)) {
+        continue;
+      }
+
+      // We want to keep track of the indices that have been contracted away. These are
+      // all the ones from the left factor, all the ones from the right factor and the one
+      // that we are contracting over right now.
+      SmallVectorRndId used;
+      merge_push_back(used, inner_rnd_id);
+      merge_push_back(used, outer_rnd_id);
+      merge_append(used, left.used_rnd_ids);
+      merge_append(used, right.used_rnd_ids);
+
+      // The right sides that we encounter at this point have the same left and right
+      // random vector indices. They may differ in the set of used random vector indices.
+      // But since we do not plan to contract the result with more DilutedFactor
+      // instances, we do not care to preserve the information about the used random
+      // vector indices. Therefore we can sum all these elements up to have less
+      // multiplications to do.
+      result_vec.push_back(
+          {typename DilutedTrace::Data{(left.data * right.data).trace()}, used});
+    }
+    LT_ULTRA_FINE_STOP;
+    LT_ULTRA_FINE_PRINT("[DilutedFactor::factor_to_trace] multiply_trace");
+  }  // for(left_vec)
+
+  if (result_vec.size() == 0) {
+    throw std::runtime_error(
+        "vector<DilutedTrace> factor_to_trace(vector<DilutedFactor>, "
+        "vector<DilutedFactor>) has an empty result.");
+  }
+
+  return result_vec;
+}
+
+std::vector<DilutedTrace> factor_to_trace(std::vector<DilutedFactor> const &vec) {
+  assert(vec.size() > 0);
+
+  std::vector<DilutedTrace> result_vec;
+
+  for (auto const &elem : vec) {
+    // We only want to use diagonal elements.
+    if (elem.ric.first != elem.ric.second) {
+      continue;
+    }
+
+    SmallVectorRndId used;
+    std::copy(std::begin(elem.used_rnd_ids),
+              std::end(elem.used_rnd_ids),
+              std::back_inserter(used));
+
+    auto const outer_rnd_id = elem.ric.first;
+    merge_push_back(used, outer_rnd_id);
+
+    DilutedTrace result = {elem.data.trace(), used};
+
+    result_vec.push_back(result);
+  }
+
+  if (result_vec.size() == 0) {
+    throw std::runtime_error(
+        "vector<DilutedTrace> factor_to_trace(vector<DilutedFactor>) has an empty "
+        "result.");
+  }
+
+  return result_vec;
+}
+
+ComplexProduct inner_product(std::vector<DilutedTrace> const &left_vec,
+                             std::vector<DilutedTrace> const &right_vec) {
+  assert(left_vec.size() > 0);
+  assert(right_vec.size() > 0);
+
+  int num_summands = 0;
+
+  ComplexProduct result(0.0, 0.0, 0.0, 0.0);
+
+  for (auto const &left : left_vec) {
+    Complex right_sum(0.0, 0.0);
+
+    for (auto const &right : right_vec) {
+      // We also need to be careful to not combine factors which have common used random
+      // vector indices.
+      if (has_intersection(left.used_rnd_ids, right.used_rnd_ids)) {
+        continue;
+      }
+
+      // The right sides that we encounter at this point have the same left and right
+      // random vector indices. They may differ in the set of used random vector indices.
+      // But since we do not plan to contract the result with more DilutedFactor
+      // instances, we do not care to preserve the information about the used random
+      // vector indices. Therefore we can sum all these elements up to have less
+      // multiplications to do.
+      right_sum += right.data;
+      ++num_summands;
+    }
+
+    result.rere += left.data.real() * right_sum.real();
+    result.reim += left.data.real() * right_sum.imag();
+    result.imre += left.data.imag() * right_sum.real();
+    result.imim += left.data.imag() * right_sum.imag();
+  }
+
+  if (num_summands == 0) {
+    throw std::runtime_error(
+        "compcomp_t inner_product(vector<DilutedTrace>, "
+        "vector<DilutedTrace>) has an empty result.");
+  }
+
+  return result / static_cast<double>(num_summands);
+}
