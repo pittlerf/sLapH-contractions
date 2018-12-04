cc7acb20acf3f7018289f8833cacc0cfbad1fb99
Martin Ueding <dev@martin-ueding.de>
2018-12-04 19:34:23 +0100

    Remove virtual function calls

--- src/Correlators.cpp
+++ src/Correlators.cpp
@@ -24,20 +24,6 @@ int get_time_delta(BlockIterator const &slice_pair, int const Lt) {
   return abs((slice_pair.sink() - slice_pair.source() - Lt) % Lt);
 }
 
-Diagram *make_diagram(char const *name,
-                      DiagramIndicesCollection const &corr_lookup,
-                      CorrelatorRequestsMap const &correlator_requests_map,
-                      std::string const &output_path,
-                      std::string const &output_filename,
-                      int const Lt) {
-  return new Diagram(corr_lookup.at(name),
-                     correlator_requests_map.at(name),
-                     output_path,
-                     output_filename,
-                     Lt,
-                     name);
-}
-
 /**
  *  @param quarklines       Instance of Quarklines. Contains prebuilt
  *                          combinations of operators and perambulators
@@ -67,7 +53,7 @@ void contract(const ssize_t Lt,
               DilutedFactorIndicesCollection const &quark_lookup,
               std::string const output_path,
               std::string const output_filename) {
-  std::vector<std::unique_ptr<Diagram>> diagrams;
+  std::vector<Diagram> diagrams;
 
   std::vector<char const *> general_diagram_names{"C20",
                                                   "C20V",
@@ -86,8 +72,12 @@ void contract(const ssize_t Lt,
 
   for (auto const &name : general_diagram_names) {
     if (!corr_lookup.at(name).empty()) {
-      diagrams.emplace_back(make_diagram(
-          name, corr_lookup, correlator_requests_map, output_path, output_filename, Lt));
+      diagrams.emplace_back(corr_lookup.at(name),
+                            correlator_requests_map.at(name),
+                            output_path,
+                            output_filename,
+                            Lt,
+                            name);
     }
   }
 
@@ -127,7 +117,7 @@ void contract(const ssize_t Lt,
 
       // Build the diagrams.
       for (auto &diagram : diagrams) {
-        if (diagram->corr_lookup().empty()) {
+        if (diagram.corr_lookup().empty()) {
           continue;
         }
         LT_CORRELATOR_START;
@@ -135,7 +125,7 @@ void contract(const ssize_t Lt,
         for (auto const slice_pair : block_pair) {
           int const t = get_time_delta(slice_pair, Lt);
 
-          diagram->assemble(t, slice_pair, q);
+          diagram.assemble(t, slice_pair, q);
         }  // End of slice pair loop.
         LT_CORRELATOR_STOP;
         LT_CORRELATOR_PRINT(std::string("[contract] ") + diagram->name());
@@ -150,6 +140,6 @@ void contract(const ssize_t Lt,
   swatch.print();
 
   for (auto &diagram : diagrams) {
-    diagram->write();
+    diagram.write();
   }
 }
