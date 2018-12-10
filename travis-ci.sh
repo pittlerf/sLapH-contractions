fe275b0eca5356d6c69cd935b8ad926640a391cf
Martin Ueding <dev@martin-ueding.de>
2018-12-10 18:41:40 +0100

    Eigen CMake fix might not be needed any more

--- travis-ci.sh
+++ travis-ci.sh
@@ -32,11 +32,11 @@ sudo apt-get install -y "${ubuntu_packages[@]}"
 #                              Fix Eigen Library                              #
 ###############################################################################
 
-sudo updatedb
-locate FindEigen3.cmake
-
-mkdir cmake-module
-cp $(locate FindEigen3.cmake) cmake-module
+#sudo updatedb
+#locate FindEigen3.cmake
+#
+#mkdir cmake-module
+#cp $(locate FindEigen3.cmake) cmake-module
 
 ###############################################################################
 #                               Install C-LIME                                #
