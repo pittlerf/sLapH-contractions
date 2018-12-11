c0e7a60127c9b3e4690cd47f5e86fa0875c5b449
Martin Ueding <dev@martin-ueding.de>
2018-12-11 17:44:03 +0100

    Create all map elements

--- include/global_data.hpp
+++ include/global_data.hpp
@@ -54,6 +54,17 @@ struct GlobalData {
     momentum_cutoff[2] = 6;
     momentum_cutoff[3] = 7;
     momentum_cutoff[4] = 4;
+
+    quarkline_lookuptable["Q0"];
+    quarkline_lookuptable["Q1"];
+    quarkline_lookuptable["Q2"];
+
+    trace_indices_map["trQ0Q2"];
+    trace_indices_map["trQ1"];
+    trace_indices_map["trQ1Q1"];
+    trace_indices_map["trQ1Q1Q1Q1"];
+    trace_indices_map["trQ2Q0Q2Q0"];
+    trace_indices_map["trQ2Q0Q2Q0Q2Q0"];
   }
 
   int Lx, Ly, Lz, Lt;
