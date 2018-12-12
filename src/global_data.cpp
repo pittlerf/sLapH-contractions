ceefe074aeb6ef3fc70c887df1980a2aca41e6c0
Martin Ueding <dev@martin-ueding.de>
2018-12-12 16:57:56 +0100

    Use range checking

--- src/global_data.cpp
+++ src/global_data.cpp
@@ -341,8 +341,8 @@ void read_parameters(GlobalData &gd, int ac, char *av[]) {
   int total_number_of_random_combinations_in_trQ1Q1 = 0;
   for (auto const &q : gd.trace_indices_map.at("trQ1Q1")) {
     total_number_of_random_combinations_in_trQ1Q1 +=
-        gd.quarkline_lookuptable.at("Q1")[q[0]].rnd_vec_ids.size() *
-        gd.quarkline_lookuptable.at("Q1")[q[1]].rnd_vec_ids.size();
+        gd.quarkline_lookuptable.at("Q1").at(q[0]).rnd_vec_ids.size() *
+        gd.quarkline_lookuptable.at("Q1").at(q[1]).rnd_vec_ids.size();
   }
   std::cout << "\ttrQ1Q1:\t" << std::fixed << std::setprecision(2)
             << gd.Lt * gd.Lt * total_number_of_random_combinations_in_trQ1Q1 *
@@ -352,8 +352,8 @@ void read_parameters(GlobalData &gd, int ac, char *av[]) {
   int total_number_of_random_combinations_in_trQ0Q2 = 0;
   for (auto const &q : gd.trace_indices_map.at("trQ0Q2")) {
     total_number_of_random_combinations_in_trQ0Q2 +=
-        gd.quarkline_lookuptable.at("Q0")[q[0]].rnd_vec_ids.size() *
-        gd.quarkline_lookuptable.at("Q2")[q[1]].rnd_vec_ids.size();
+        gd.quarkline_lookuptable.at("Q0").at(q[0]).rnd_vec_ids.size() *
+        gd.quarkline_lookuptable.at("Q2").at(q[1]).rnd_vec_ids.size();
   }
   std::cout << "\ttrQ0Q2:\t" << std::fixed << std::setprecision(2)
             << gd.Lt * gd.Lt * total_number_of_random_combinations_in_trQ0Q2 *
@@ -363,7 +363,7 @@ void read_parameters(GlobalData &gd, int ac, char *av[]) {
   int total_number_of_random_combinations_in_trQ1 = 0;
   for (auto const &q : gd.trace_indices_map.at("trQ1")) {
     total_number_of_random_combinations_in_trQ1 +=
-        gd.quarkline_lookuptable.at("Q1")[q[0]].rnd_vec_ids.size();
+        gd.quarkline_lookuptable.at("Q1").at(q[0]).rnd_vec_ids.size();
   }
   std::cout << "\ttrQ1:\t" << std::fixed << std::setprecision(2)
             << gd.Lt * total_number_of_random_combinations_in_trQ1 * sizeof(Complex) /
