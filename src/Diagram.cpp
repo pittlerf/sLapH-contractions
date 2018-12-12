bf65650cd34d4a21e5d28125f6d8ceb67c86d036
Martin Ueding <dev@martin-ueding.de>
2018-12-12 13:52:31 +0100

    Implement diagrams with three traces

--- src/Diagram.cpp
+++ src/Diagram.cpp
@@ -46,6 +46,34 @@ ComplexProduct resolve_request2(std::vector<TraceRequest> const &trace_requests,
   return inner_product(t0, t1);
 }
 
+ComplexProduct resolve_request3(std::vector<TraceRequest> const &trace_requests,
+                                BlockIterator const &slice_pair,
+                                DiagramParts &q) {
+  assert(ssize(trace_requests) == 2);
+  auto const &trace_request0 = trace_requests.at(0);
+  auto const &locations0 = trace_request0.locations;
+  auto const &x0 = q.trace_factories.at(trace_request0.tr_name)
+                       ->get(slice_pair, locations0)
+                       .at(trace_request0.tr_id);
+  DilutedTraces t0{x0, false};
+
+  auto const &trace_request1 = trace_requests.at(1);
+  auto const &locations1 = trace_request1.locations;
+  auto const &x1 = q.trace_factories.at(trace_request1.tr_name)
+                       ->get(slice_pair, locations1)
+                       .at(trace_request1.tr_id);
+  DilutedTraces t1{x1, false};
+
+  auto const &trace_request2 = trace_requests.at(2);
+  auto const &locations2 = trace_request2.locations;
+  auto const &x2 = q.trace_factories.at(trace_request2.tr_name)
+                       ->get(slice_pair, locations2)
+                       .at(trace_request2.tr_id);
+  DilutedTraces t2{x2, false};
+
+  return inner_product(t0 * t1, t2);
+}
+
 ComplexProduct resolve_request(std::vector<TraceRequest> const &trace_requests,
                                BlockIterator const &slice_pair,
                                DiagramParts &q) {
@@ -53,6 +81,8 @@ ComplexProduct resolve_request(std::vector<TraceRequest> const &trace_requests,
     return resolve_request1(trace_requests, slice_pair, q);
   } else if (ssize(trace_requests) == 2) {
     return resolve_request2(trace_requests, slice_pair, q);
+  } else if (ssize(trace_requests) == 3) {
+    return resolve_request3(trace_requests, slice_pair, q);
   } else {
     throw std::runtime_error("This many traces are not implemented yet.");
   }
