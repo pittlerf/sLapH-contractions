8e99a86717268ff8a592c85de5dc9e2e658c65d8
Martin Ueding <dev@martin-ueding.de>
2019-05-20 19:52:28 +0200

    Add an untested DilutedProductFactoryQ0Q2

--- include/DilutedProductFactory.hpp
+++ include/DilutedProductFactory.hpp
@@ -1,3 +1,33 @@
 #pragma once
 
 #include "DilutedFactor.hpp"
+#include "DilutedFactorFactory.hpp"
+
+class DilutedProductFactoryQ0Q2 {
+ public:
+  using Key = std::array<int, 4>;
+  using Value = DilutedFactorsMap<2>;
+
+  DilutedProductFactoryQ0Q2(DilutedFactorFactory<DilutedFactorType::Q0> &factory_q0,
+                            DilutedFactorFactory<DilutedFactorType::Q2> &factory_q2)
+      : factory_q0_(factory_q0), factory_q2_(factory_q2) {}
+
+  std::vector<DilutedFactor> const &get(Key const &time_key,
+                                        std::array<ssize_t, 2> const &key) {
+    if (Q0Q2_.count(time_key) == 0 || Q0Q2_[time_key].count(key) == 0) {
+      build(time_key, key);
+    }
+
+    return Q0Q2_.at(time_key).at(key);
+  }
+
+  void clear() { Q0Q2_.clear(); }
+
+  void build(Key const &time_key, std::array<ssize_t, 2> const &key);
+
+ private:
+  std::map<Key, Value> Q0Q2_;
+
+  DilutedFactorFactory<DilutedFactorType::Q0> &factory_q0_;
+  DilutedFactorFactory<DilutedFactorType::Q2> &factory_q2_;
+};
--- src/DilutedProductFactory.cpp
+++ src/DilutedProductFactory.cpp
@@ -1,1 +1,15 @@
 #include "DilutedProductFactory.hpp"
+
+void DilutedProductFactoryQ0Q2::build(Key const &time_key,
+                                      std::array<ssize_t, 2> const &key) {
+  // Extract time keys.
+  int constexpr nt1 = 1;
+  int constexpr nt2 = 3;
+  std::array<int, nt1> time_key1;
+  std::array<int, nt2> time_key2;
+  std::copy_n(std::begin(time_key) + 0, nt1, std::begin(time_key1));
+  std::copy_n(std::begin(time_key) + nt1, nt2, std::begin(time_key2));
+
+  multiply<1, 1>(
+      Q0Q2_[time_key], key, factory_q0_[time_key1], factory_q2_[time_key2]);
+}
